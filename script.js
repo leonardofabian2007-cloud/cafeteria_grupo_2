352f187f74b870ded075cc4240c7ca53a9cafb16
Juan Botia <juanes150307@gmail.com>
2026-06-09 11:16:04 -0500

    Add event listeners for cafe and postre buttons

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const navbar = document.getElementById;
+const postres = document.querySelectorAll(".postre");
+const cafes = document.querySelectorAll(".cafe");
+
+const btnCafes = document.getElementById("btn-cafes");
+btnCafes.addEventListener("click", () => {
+  postres.forEach((postre) => {
+    postre.style.display = "none";
+  });
+
+  cafes.forEach((cafe) => {
+    cafe.style.display = "block";
+  });
+});
+
+const btnPostres = document.getElementById("btn-postres");
+btnCafes.addEventListener("click", () => {
+  cafes.forEach((cafe) => {
+    cafe.style.display = "none";
+  });
+
+  postres.forEach((postre) => {
+    postre.style.display = "block";
+  });
+});
